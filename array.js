98e3c6e56b1c4025af4f2d81864c07ddc3c71388
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-07-04 07:52:40 +0430

    nondistructive functions on arrays

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 let shoppingList = ['eggs', 'bread', 'milk', 'juice'];
+let result;
 console.log("Shopping List", shoppingList)
 
 // add 'potato' to the end of shoppingList
@@ -16,3 +17,22 @@ console.log("remove the last item of shopping list", shoppingList)
 //remove the first element from shoppingList
 shoppingList.shift()
 console.log("remove the first item of shopping list", shoppingList)
+
+//copy of shopping list
+result = shoppingList.slice()
+
+//remove the first element nondestructively(without changing shopping list)
+result = shoppingList.slice(1)
+
+//remove the last element nondestructively(without changing shopping list)
+result = shoppingList.slice(0, shoppingList.length - 1)
+
+//add garlic to the end of shopping list nondestructively
+result = [...shoppingList, 'garlic']
+
+//add onion to the beginning of the shopping list nondestructively
+result = ['onion', ...shoppingList]
+
+// console.log("shopping list",shoppingList)
+// console.log("result", result)
+
