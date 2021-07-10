d5705d138b0d4cb89c6a02f48638c8cf2a7cd9cf
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-07-10 11:00:33 +0430

    prevent to add duplicate task

--- todo.js
+++ todo.js
@@ -8,18 +8,32 @@ createTaskForm.addEventListener('submit', function(e){
     message.textContent = ""
    
     if (newTaskDescription.value) {
-        addNewTask()
+        if(!checkDuplication()){
+            addNewTask()
+        } else {
+            message.textContent = "Task is exist!"
+        }
     }
     else {
         message.textContent = "Please enter your task description!"
     }
 })
 
+function checkDuplication(){
+    const existingTasks = document.getElementsByTagName('li')
+    for(let i = 0; i < existingTasks.length; i++){
+        if(existingTasks[i].textContent.split(' ')[0] == newTaskDescription.value) {
+            return true
+        }
+    }
+    return false
+}
+
 function addNewTask() {
     let task = document.createElement('li')
     task.textContent = newTaskDescription.value
     let deleteBtn = document.createElement('bottun')
-    deleteBtn.textContent = "   X"
+    deleteBtn.textContent = " X"
     deleteBtn.style.color = "red"
     task.appendChild(deleteBtn)
     tasks.appendChild(task)
