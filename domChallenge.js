0ce1f6cf0ad6b9f86fd79a2737bb97118ccdfcd9
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-07-12 11:12:53 +0430

    disable bottuns with pause

--- domChallenge.js
+++ domChallenge.js
@@ -2,6 +2,8 @@ let counter = document.getElementById('counter')
 const pause = document.getElementById('pause')
 const minus = document.getElementById('minus')
 const plus = document.getElementById('plus')
+const heart = document.getElementById('heart')
+const message = document.getElementById('message')
 let timer;
 
 document.addEventListener('DOMContentLoaded', startCounter)
@@ -16,7 +18,21 @@ function startCounter(){
 }
 
 function pauseCounter(){
-    clearInterval(timer)
+    if(pause.innerText == "pause"){
+        disableButtons(true)
+        pause.innerText = "Resume"
+        clearInterval(timer)
+    } else {
+        disableButtons(false)
+        pause.innerText = "pause"
+        startCounter()
+    }
+}
+
+function disableButtons(disable) {
+    minus.disabled = disable
+    plus.disabled = disable
+    heart.disabled = disable
 }
 
 function decreaseCounter() {
@@ -26,5 +42,3 @@ function decreaseCounter() {
 function increaseCounter() {
     counter.innerHTML = parseInt(counter.innerHTML, 10) + 1;
 }
-
-
