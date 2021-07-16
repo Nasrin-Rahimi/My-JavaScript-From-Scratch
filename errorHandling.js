f7699607cd95603baf9128741f645ad369179701
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-07-16 16:50:54 +0430

    throw new Error

--- errorHandling.js
+++ errorHandling.js
@@ -6,10 +6,19 @@ submitBtn.addEventListener('click', checkInput)
 
 function checkInput() {
     message.innerText = ""
+    // try { if(input.value < 5) {
+    //     //We can throw strings, numbers, booleans or objects. 
+    //     // If no error is thrown the catch statement is skipped.
+    //     throw 'Input is too small!'
+    // }
+    // } catch(error) {
+    //     message.innerText = error
+    // }
     try { if(input.value < 5) {
-        throw 'Input is too small!'
-    }
+        //Throwing a new Error requires a single argument of a message. 
+        throw new Error('Input is too small')
+        }   
     } catch(error) {
-        message.innerText = error
+        message.innerText = error.message
     }
 }
