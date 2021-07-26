664b37af2d60a0ef7f722318f518b89de55aab72
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-07-26 11:10:33 +0430

    check time complexity for inclusion

--- timeComplexity.js
+++ timeComplexity.js
@@ -3,5 +3,20 @@ const letter = document.getElementById('letter')
 const message = document.getElementById('message')
 
 function checkInclusion() {
-   sentence.value.includes(letter.value) ? message.innerHTML = 'true' : message.innerHTML = 'false'
+//    sentence.value.includes(letter.value) ? message.innerHTML = 'true' : message.innerHTML = 'false'
+    message.innerHTML = stringIncludes(sentence.value, letter.value)
 }
+
+function stringIncludes(sentence, letter){
+    let matches; //1
+    for (let i = 0; i < sentence.length; i++) { //2
+        if(sentence[i] === letter) { // 3 (in the worth case sentence.length
+            matches = true //4
+        }
+    }
+
+    return !!matches //5
+}
+
+//if the letter doesn't exist in sentence, it takes n + 3 times for searching letter in sentence
+//n is sentence.length. we call time copmlexity of stringIncludes function is n + 3
