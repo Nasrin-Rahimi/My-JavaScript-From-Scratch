bbc288ab85bbf8a3a137676b0e003d94863c5491
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-26 13:20:54 +0430

    sort three numbers function

---
+++ FoundationFunctions.js
@@ -0,0 +1,21 @@
+function sortThreeNumber(a, b, c){
+    let array = []
+    let tmp = Math.min(a, b)
+    b = Math.max(a, b)
+    a = tmp
+    array.push(a)
+    tmp = Math.min (b, c)
+    c = Math.max(b, c)
+    b = tmp
+    array.push(b)
+    tmp = Math.min(a, c)
+    c = Math.max(a, c)
+    a = tmp
+    array.push(c)
+    return array
+}
+
+function evenlySpaced(a, b, c){
+
+}
+
