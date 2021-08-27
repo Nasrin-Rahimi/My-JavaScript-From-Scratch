6fdedfcf13551dc8c5799a8df5f1f47bae30d9f9
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-27 07:17:58 +0430

    evenly space function

--- FoundationFunctions.js
+++ FoundationFunctions.js
@@ -16,6 +16,18 @@ function sortThreeNumber(a, b, c){
 }
 
 function evenlySpaced(a, b, c){
-
+    let array = []
+    let tmp = Math.min(a, b)
+    b = Math.max(a, b)
+    a = tmp
+    array.push(a)
+    tmp = Math.min (b, c)
+    c = Math.max(b, c)
+    b = tmp
+    array.push(b)
+    tmp = Math.min(a, c)
+    c = Math.max(a, c)
+    a = tmp
+    return (a - b) == (b - c)
 }
 
