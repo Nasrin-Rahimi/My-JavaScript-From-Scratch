2109926a3c8b541795c12e2ea2f01fef48a0a743
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-27 07:35:35 +0430

    make bricks

--- FoundationFunctions.js
+++ FoundationFunctions.js
@@ -15,6 +15,9 @@ function sortThreeNumber(a, b, c){
     return array
 }
 
+//Given three ints, a b c, one of them is small, one is medium and one is large. 
+//Return true if the three values are evenly spaced, so the difference between 
+//small and medium is the same as the difference between medium and large.
 function evenlySpaced(a, b, c){
     let array = []
     let tmp = Math.min(a, b)
@@ -29,5 +32,13 @@ function evenlySpaced(a, b, c){
     c = Math.max(a, c)
     a = tmp
     return (a - b) == (b - c)
+}
+
+//We want to make a row of bricks that is goal inches long. We have a number of 
+//small bricks (1 inch each) and big bricks (5 inches each). Return True if it 
+//is possible to make the goal by choosing from the given bricks. 
+
+function makeBricks(small, big, goal){
+    return ((goal % 5 <= small) && ((goal - (big * 5)) <= small))
 }
 
