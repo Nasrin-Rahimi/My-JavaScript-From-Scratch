12b2f51f6958b8153ee583c6109ba39870a2b9a2
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-28 08:22:04 +0430

    blackjack

--- FoundationFunctions.js
+++ FoundationFunctions.js
@@ -42,3 +42,43 @@ function makeBricks(small, big, goal){
     return ((goal % 5 <= small) && ((goal - (big * 5)) <= small))
 }
 
+function makeBricks2(s, b, g){
+    if((g < 5) && (s >= g)){
+        return true
+    } else if((g == 5) && (b >= 1)){
+        return true
+    } else if((g > 5) && b*5 <= g && s == g - 5*g ){
+        return true
+    } else if(g > 5 && s >= g - 5*b){
+    return true
+    }
+    else{
+        return false
+
+    }
+
+}
+
+//Given 2 int values greater than 0, return whichever value is nearest to 21 without
+// going over. Return 0 if they both go over.
+
+//blackjack(19, 21) → 21
+//blackjack(21, 19) → 21
+//blackjack(19, 22) → 19
+
+function blackJack(a, b) {
+    if (a > 21 && b > 21) {
+        return 0
+    } else if(a > 21){
+        return b
+    } else if (b > 21) {
+        return a
+    } else if ((21 - a) < (21 - b)) {
+        return a
+    } else {
+        return b
+    }
+}
+
+
+
