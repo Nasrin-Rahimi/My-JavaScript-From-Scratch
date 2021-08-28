4709c2cf8d00293ed70f833d295b2cee432158a4
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-28 12:36:13 +0430

    function declaration and function expression

--- FoundationFunctions.js
+++ FoundationFunctions.js
@@ -148,3 +148,5 @@ function firstReverse(str) {
 
 
 
+
+
---
+++ function.js
@@ -0,0 +1,7 @@
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday I want to ${activity}`)
+}
+
+let mondayWork = function(activity = 'go to the office') {
+    console.log(`This Monday, I will ${activity}`)
+}
