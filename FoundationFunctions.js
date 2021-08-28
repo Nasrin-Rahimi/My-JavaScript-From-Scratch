d038fff0bca2a9db1a1ceb37e2558542d020a7e8
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-08-28 08:40:49 +0430

    array123

--- FoundationFunctions.js
+++ FoundationFunctions.js
@@ -80,5 +80,24 @@ function blackJack(a, b) {
     }
 }
 
+//Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears 
+//in the array somewhere.
+
+//array123([1, 1, 2, 3, 1]) → True
+//array123([1, 1, 2, 4, 1]) → False
+//array123([1, 1, 2, 1, 2, 3]) → True
+
+function array123(nums) {
+    let result = false
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3){
+            result = true
+            break;
+        }
+    }
+
+    return result;
+}
+
 
 
