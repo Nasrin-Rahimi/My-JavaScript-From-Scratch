64c00af13ab7ea4b9b672bf4f42c6998253da8c7
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-02 08:41:36 +0430

    find intersection in two array

--- array.js
+++ array.js
@@ -36,4 +36,68 @@ result = ['onion', ...shoppingList]
 // console.log("shopping list",shoppingList)
 // console.log("result", result)
 
+//take the array of numbers stored in arr and return the string true if any 
+//combination of numbers in the array (excluding the largest number) can be 
+//added up to equal the largest number in the array
+
+function arraAddition(arr) {
+    arr = arr.sort(function(a, b) { a - b; });
+    let greatest = arr.pop();
+
+    function search(sum, index) {
+        if(index > arr.length) {
+            return false;
+        }
+        if(sum + arr[index] === greatest) {
+            return true;
+        }
+        else if(sum + arr[index] < greatest) {
+            return search(sum, index + 1) || search(sum + arr[index], index + 1);
+        } else {
+            return false;    
+        }
+    }
+
+    return search(0, 0);
+}
+
+
+let arr = [1,7,5,5,16];
+console.log("arrayAddition result is: ", arraAddition(arr));
+
+//Have strArr which will contain 2 elements: the first element will represent
+// a list of comma-separated numbers sorted in ascending order, the second 
+//element will represent a second list of comma-separated numbers (also sorted). 
+//Your goal is to return a comma-separated string containing the numbers that 
+//occur in elements of strArr in sorted order. If there is no intersection, 
+//return the string false.
+//Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+//Output: 1,4,13
+
+function FindIntersection(strArr) {
+    let resultStr = " ";
+    let arr1, arr2;
+
+    arr1 = strArr[0].split(',');
+    arr2 = strArr[1].split(',');
+
+    for(let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr2.length; j++) {
+         
+          if (parseInt(arr1[i]) === parseInt(arr2[j])){
+            resultStr += arr1[i] + ',';
+             break;
+          }
+        }
+      }
+    
+      if(resultStr != " ") {
+        return resultStr.slice(0, -1); 
+      } else {
+        return false;
+      }
+    
+}
+
+console.log(FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]));
 
