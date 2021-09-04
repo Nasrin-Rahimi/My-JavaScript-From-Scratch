9326d5bfe1bdf504180f17ce775efb23da55321f
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-09-04 07:49:57 +0430

    stack functions

--- stack.js
+++ stack.js
@@ -2,4 +2,37 @@ class Stack{
     constructor() {
         this.items = [];
     }
+
+    //adds an element at the top of the stack.
+    push (element) {
+        this.items.push(element);
+    }
+
+    pop () {
+        if(this.isEmpty()) {
+            return 'Underflow';
+        }
+        return this.items.pop();
+    }
+
+    // return the top most element from the stack
+    getElement() {
+        return this.items[this.items.length - 1];
+    }
+
+    //Helper Methods
+
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    printStack() {
+        let output = "";
+
+        for(let i = 0; i < this.items.length; i++) {
+            output += this.items[i] + " ";
+        }
+
+        return output;
+    }
 }
